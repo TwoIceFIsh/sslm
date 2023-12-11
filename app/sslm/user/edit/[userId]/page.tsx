1507e0fdc9bdabab789af6c97fea3daacf8f534b
이병호 <team.k0konutz@gmail.com>
2023-12-11 11:01:58 +0900

    ADD user Edit

---
+++ app/sslm/user/edit/[userId]/page.tsx
@@ -0,0 +1,117 @@
+'use client';
+import { toast } from 'react-hot-toast';
+import UserInfo from '@/app/types/UserInfo';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Page = ({ params }: { params: { userId: string } }) => {
+    const [user, setUser] = useState<UserInfo>({
+        userEmail: '',
+        userName: '',
+        userPassword: '',
+        userId: '',
+    });
+
+    const router = useRouter();
+
+    const saveUserInfo = async () => {
+        if (user.userPassword === '') {
+            toast.error('비밀번호를 입력해주세요.');
+            return;
+        }
+
+        // Perform any necessary validation before saving the user info
+
+        const data = await fetch('/api/sslm/user/update', {
+            method: 'POST',
+            body: JSON.stringify(user),
+        });
+        const result = await data.json();
+        if (result.result) {
+            toast.success('회원 수정 성공');
+            router.push('/sslm/user');
+        } else toast.error('회원 수정 실패');
+    };
+
+    const getUserInfo = async () => {
+        const res = await fetch(`/api/sslm/user/${params.userId}`);
+        const data = await res.json();
+        setUser(data);
+    };
+
+    useEffect(() => {
+        getUserInfo();
+    }, []);
+
+    return (
+        <div
+            className={
+                'fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-100 bg-opacity-30'
+            }
+        >
+            <div
+                className={
+                    'h-54 flex w-64 flex-col justify-center gap-4  border bg-white p-4 text-xs'
+                }
+            >
+                {/* ... Other input fields */}
+                <div className={'flex justify-between border'}>
+                    <div className={'w-full flex-col bg-red-100 text-center'}>이메일</div>
+                    <div className={'mx-2 w-full flex-col bg-gray-100'}>
+                        <input
+                            name="userEmail"
+                            value={user?.userEmail}
+                            disabled={true}
+                            placeholder={'이메일'}
+                            onChange={(e) => setUser({ ...user, userEmail: e.target.value })}
+                        />
+                    </div>
+                </div>
+
+                <div className={'flex justify-between border'}>
+                    <div className={'w-full flex-col bg-red-100 text-center'}>이름</div>
+                    <div className={'mx-2 w-full flex-col bg-gray-100'}>
+                        <input
+                            name="userName"
+                            defaultValue={user?.userName}
+                            placeholder={'이름'}
+                            onChange={(e) => setUser({ ...user, userName: e.target.value })}
+                        />
+                    </div>
+                </div>
+                <div className={'flex justify-between border'}>
+                    <div className={'w-full flex-col bg-red-100 text-center'}>비밀번호</div>
+                    <div className={'mx-2 w-full flex-col bg-gray-100'}>
+                        <input
+                            name="userPassword"
+                            type={'password'}
+                            defaultValue={''}
+                            placeholder={'비밀번호'}
+                            onChange={(e) => setUser({ ...user, userPassword: e.target.value })}
+                        />
+                    </div>
+                </div>
+
+                <div className={'flex items-center justify-center gap-2'}>
+                    <div
+                        className={
+                            'w-12 cursor-pointer select-none rounded-md border bg-green-300 p-1 text-center font-bold'
+                        }
+                        onClick={() => saveUserInfo()}
+                    >
+                        확인
+                    </div>
+                    <div
+                        className={
+                            'w-12 cursor-pointer select-none rounded-md border bg-yellow-300 p-1 text-center font-bold'
+                        }
+                    >
+                        취소
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
